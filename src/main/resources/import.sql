001150b09c72933c475e8bcf101dba58423881b5
artorresb <artorresb@ARTORRESB10.indra.es>
2020-01-06 20:57:41 +0100

    datos iniciales

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
 /* Populate tables */
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Álvaro', 'Torres', 'atbotella@gmail.com', '2020-01-07');
-INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Juan', 'Español', 'juanespa@gmail.com', '2020-01-07');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Alvaro', 'Torres', 'atbotella@gmail.com', '2020-01-07');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'nombre', 'apellido', 'mail@gmail.com', '2020-01-07');

61406b70e30ed5c491f7a5da5d4406b34462a2cb
Torres Botella <artorresb@minsait.com>
2020-01-06 19:45:02 +0100

    subida inicial

---
+++ src/main/java/com/atb/springboot/app/models/dao/IClienteDao.java
@@ -0,0 +1,10 @@
+package com.atb.springboot.app.models.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.atb.springboot.app.models.entity.Cliente;
+
+public interface IClienteDao extends CrudRepository<Cliente, Long>{
+
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(1, 'Álvaro', 'Torres', 'atbotella@gmail.com', '2020-01-07');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(2, 'Juan', 'Español', 'juanespa@gmail.com', '2020-01-07');
